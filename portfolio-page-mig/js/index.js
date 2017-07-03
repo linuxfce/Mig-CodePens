cea7d6368a6c02e6af102db280ecdb21ba81578a
Meireles <linuxfce@users.noreply.github.com>
2017-07-03 16:27:00 +0100

    Add files via upload

---
+++ portfolio-page-mig/js/index.js
@@ -0,0 +1,11 @@
+$(function(){
+  
+  var backgroundImg = [
+    'https://images.unsplash.com/photo-1483125078247-1a358ff363d3?dpr=1.2000000476837158&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg=',
+'https://docs.google.com/uc?id=0B8LPYClxJKSNSkVWVHJ1TWcxQkk'
+  
+  ];
+  
+  $('body').css('background-image', 'url(' + backgroundImg[1] + ')');
+  
+})
